e205e4c54a876134ca3f9cc7b1869d69fb41eed7
Benit Polvie <polviedivine@gmail.com>
2025-07-23 19:52:18 +0200

    Delete twttr.py

--- twttr.py
+++
@@ -1,9 +0,0 @@
-def main():
-    user_input = input('Input: ')
-    print(twttr(user_input))
-
-def twttr(string):
-    return ''.join([i if i not in "aeiouAEIOU" else "" for i in string])
-
-if __name__ == "__main__":
-    main()
